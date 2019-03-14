0b48b24ecaa5d0eea061618058cbe580e58a182d
Jolt64 <jonathantelford64@gmail.com>
2019-03-14 10:16:08 -0600

    things

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      things: [
+        'cell phone','soy sauce packet','stockings','pants','flowers','clothes','tv','apple','sketch pad'
+      ],
+      userInput: '',
+      filteredThings: []
+    }
+  }
+  
+  // handleChange = (e) => {
+  //   this.setState({
+  //     userInput: e.target.value
+  //   })
+  // }
+  
+  searchArr = (e) => {
+    let { things} = this.state
+    let freshFilteredThings = things.filter(word => word.startsWith(e.target.value))
+    console.log('hit');
+    
+    this.setState({
+      filteredThings: freshFilteredThings
+    })
+  }
+  
+  
+  
   render() {
+    let holder = []
+    if(this.state.filteredThings) {
+      holder = this.state.filteredThings.map((thing, i) => <h4 key={i}>{thing}</h4>)
+    } else {
+      holder = this.state.things.map((thing, i) => <h4 key={i}>{thing}</h4>)
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={(e) => this.searchArr(e)}></input>
+      {holder}
       </div>
     );
   }
